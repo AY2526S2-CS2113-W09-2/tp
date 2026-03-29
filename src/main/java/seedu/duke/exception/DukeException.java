4d131ce6d496f1b4864f93eea3ce44db022965e0
LUKE\lukel <lukelousg@gmail.com>
2026-03-29 14:27:33 +0800

    DukeException Java Doc

--- src/main/java/seedu/duke/exception/DukeException.java
+++ src/main/java/seedu/duke/exception/DukeException.java
@@ -1,6 +1,17 @@
 package seedu.duke.exception;
 
+/**
+ * Represents an exception specific to the Duke application.
+ * It is used to signal command, parsing, or validation errors
+ * that should be shown to the user.
+ */
 public class DukeException extends Exception {
+
+    /**
+     * Creates a Duke exception with the specified error message.
+     *
+     * @param message Message describing the error.
+     */
     public DukeException(String message) {
         super(message);
     }
